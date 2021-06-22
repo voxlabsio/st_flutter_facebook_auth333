9341f8086293e7b178c04daff5445fc0e4f47df6
hackerhgl <hackerhgl@gmail.com>
2021-06-22 19:26:31 +0500

    park

---
+++ example/lib/pages/splash/splash_controller.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+
+class SplashController extends ChangeNotifier {
+  final FacebookAuth _facebookAuth;
+  bool? _isLogged;
+  bool _fetching = false;
+
+  bool get fetching => _fetching;
+  bool? get isLogged => _isLogged;
+
+  Map<String, dynamic>? _userData;
+  Map<String, dynamic>? get userData => _userData;
+
+  SplashController(this._facebookAuth) {
+    _init();
+  }
+
+  void _init() async {
+    _isLogged = await this._facebookAuth.accessToken != null;
+    if (_isLogged!) {
+      _userData = await _facebookAuth.getUserData();
+    }
+    notifyListeners();
+  }
+
+  Future<bool> login() async {
+    _fetching = true;
+    notifyListeners();
+    final result = await _facebookAuth.login();
+
+    _isLogged = result.status == LoginStatus.success;
+    if (_isLogged!) {
+      _userData = await _facebookAuth.getUserData();
+    }
+    _fetching = false;
+    notifyListeners();
+    return _isLogged!;
+  }
+
+  Future<void> logout() async {
+    _fetching = true;
+    notifyListeners();
+    await _facebookAuth.logOut();
+    _fetching = false;
+    _isLogged = false;
+    notifyListeners();
+  }
+}
